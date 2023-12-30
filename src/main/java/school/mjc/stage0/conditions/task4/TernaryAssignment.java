4a56cd1ca0949d7e6c2bcea856654a0b26cc6ad9
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-30 12:13:14 +0100

    resolved

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -5,8 +5,7 @@ public class TernaryAssignment {
 
     }
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
-        int result;
-        result = first > second  ? 10 : -10;
+        int result = first > second  ? 10 : -10;
         System.out.println(result);
     }
 }
